d17969e9a90ca777d3cf384867d04b17e3b30f09
Jonne Pihlanen <jonne.pihlanen@futurice.com>
2022-01-05 22:21:42 +0200

    remove aur helper & add updated packages.txt.

--- install.sh
+++ install.sh
@@ -3,19 +3,10 @@
 git submodule init
 git submodule update
 
-# Install AUR helper
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-rm -rf yay
-
-# Install packages
-yay -S reflector
+pacman -S reflector
 sudo reflector --save /etc/pacman.d/mirrorlist --sort rate -c Finland -n 15 -p https
 
-yay -Syu
-yay -S $(< packages.txt)
+pacman -Syu --needed $(< packages.txt)
 
 for f in */install.sh; do
 	$f

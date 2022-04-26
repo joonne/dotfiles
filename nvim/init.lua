efc7498786a009275941b4c781870433db44364e
Jonne Pihlanen <jonne.pihlanen@futurice.com>
2022-04-26 08:08:21 +0300

    nvim: add nerdtree & lspconfig.

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,8 +2,9 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
-Plug 'tpope/vim-sensible'
-Plug 'preservim/nerdtree'
+Plug('tpope/vim-sensible')
+Plug('scrooloose/nerdtree', { on = 'NERDTreeToggle' })
+Plug('neovim/nvim-lspconfig')
 
 vim.call('plug#end')
 

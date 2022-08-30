f7d94ee72424b84cdf4b29a13c41be5503e56685
Jonne Pihlanen <jonne.pihlanen@futurice.com>
2022-08-30 17:26:57 +0300

    electron: native wayland for electron17 apps.

--- electron/install.sh
+++ electron/install.sh
@@ -3,4 +3,5 @@
 set -euo pipefail
 
 ln -sf "$(pwd)/electron/electron-flags.conf" "$HOME/.config/electron-flags.conf"
+ln -sf "$(pwd)/electron/electron-flags.conf" "$HOME/.config/electron17-flags.conf"
 

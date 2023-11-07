846eae45eb4198835de49c280404ccf9f1241957
Jonne Pihlanen <jonne.pihlanen@sleek.works>
2023-11-07 09:32:45 +0200

    feat(nvim/telescope): add mapping for telescope's buffers

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,4 @@ vim.keymap.set('n', '<leader>f', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})

3220d2173fb61b8cbe5ac299932e80c4e3d948f1
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2024-02-07 10:36:06 +0200

    feat(nvim): add ng & arduino plugins

---
+++ nvim/after/plugin/ng.lua
@@ -0,0 +1,5 @@
+local opts = { noremap = true, silent = true }
+local ng = require("ng");
+vim.keymap.set("n", "<leader>at", ng.goto_template_for_component, opts)
+vim.keymap.set("n", "<leader>ac", ng.goto_component_with_template_file, opts)
+vim.keymap.set("n", "<leader>aT", ng.get_template_tcb, opts)
--- nvim/lua/joonne/packer.lua
+++ nvim/lua/joonne/packer.lua
@@ -1,48 +1,50 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  use { 'nvim-telescope/telescope.nvim', tag = '0.1.4', requires = { {'nvim-lua/plenary.nvim'} } }
+    use { 'nvim-telescope/telescope.nvim', tag = '0.1.4', requires = { { 'nvim-lua/plenary.nvim' } } }
 
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		    end,
-        },
-	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                            -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-	    -- Autocompletion
-	    {'hrsh7th/nvim-cmp'},     -- Required
-	    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	    {'L3MON4D3/LuaSnip'},     -- Required
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
     }
-  }
-  use 'github/copilot.vim'
-  use 'airblade/vim-gitgutter'
-  use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
-  use 'mfussenegger/nvim-dap'
-  use 'elentok/format-on-save.nvim'
-  use 'kylechui/nvim-surround'
-  use 'm4xshen/autoclose.nvim'
-  use 'terrortylor/nvim-comment'
-    end)
+    use 'github/copilot.vim'
+    use 'airblade/vim-gitgutter'
+    use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
+    use 'mfussenegger/nvim-dap'
+    use 'elentok/format-on-save.nvim'
+    use 'kylechui/nvim-surround'
+    use 'm4xshen/autoclose.nvim'
+    use 'terrortylor/nvim-comment'
+    use 'stevearc/vim-arduino'
+    use 'joeveiga/ng.nvim'
+end)

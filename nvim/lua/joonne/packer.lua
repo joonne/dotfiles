c29f6826ff4109bf9f49ce463c5c3395f3c66d4a
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2025-10-23 23:06:40 +0300

    feat(nvim): replace format-on-save with conform, remove fugitive

--- nvim/lua/joonne/packer.lua
+++ nvim/lua/joonne/packer.lua
@@ -1,58 +1,57 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/mason.nvim'
-    use 'williamboman/mason-lspconfig.nvim'
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
 
-    use { 'nvim-telescope/telescope.nvim', tag = '0.1.4', requires = { { 'nvim-lua/plenary.nvim' } } }
+	use("neovim/nvim-lspconfig")
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
 
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
+	use({ "nvim-telescope/telescope.nvim", tag = "0.1.4", requires = { { "nvim-lua/plenary.nvim" } } })
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use 'github/copilot.vim'
-    use 'airblade/vim-gitgutter'
-    use {
-        'akinsho/bufferline.nvim',
-        tag = "*",
-        requires = 'nvim-tree/nvim-web-devicons'
-    }
-    use 'mfussenegger/nvim-dap'
-    use 'elentok/format-on-save.nvim'
-    use 'kylechui/nvim-surround'
-    use 'm4xshen/autoclose.nvim'
-    use 'terrortylor/nvim-comment'
-    use 'stevearc/vim-arduino'
-    use 'joeveiga/ng.nvim'
-    use 'lewis6991/gitsigns.nvim'
-    use({
-        "olimorris/codecompanion.nvim",
-        config = function()
-            require("codecompanion").setup()
-        end,
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-        }
-    })
-    use({
-        "ravitemer/mcphub.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        build = "npm install -g mcp-hub@latest",
-        config = function()
-            require("mcphub").setup()
-        end
-    })
+	use({
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	})
+
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("mbbill/undotree")
+	use("github/copilot.vim")
+	use("airblade/vim-gitgutter")
+	use({
+		"akinsho/bufferline.nvim",
+		tag = "*",
+		requires = "nvim-tree/nvim-web-devicons",
+	})
+	use("mfussenegger/nvim-dap")
+	use("kylechui/nvim-surround")
+	use("m4xshen/autoclose.nvim")
+	use("terrortylor/nvim-comment")
+	use("stevearc/vim-arduino")
+	use("joeveiga/ng.nvim")
+	use("lewis6991/gitsigns.nvim")
+	use({
+		"olimorris/codecompanion.nvim",
+		config = function()
+			require("codecompanion").setup()
+		end,
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+	})
+	use({
+		"ravitemer/mcphub.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		build = "npm install -g mcp-hub@latest",
+		config = function()
+			require("mcphub").setup()
+		end,
+	})
+	use("stevearc/conform.nvim")
 end)
